d8e103191b348647e85f03744da1f4cf5924dc02
Walter <walter.salas@me.com>
2020-06-16 15:46:05 -0500

    first build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { resolve } = require('path');
+//Configuración
+//Trae path lo cuaĺ nos permite saber donde nos estamos moviendo en el proeycto
+const path = require('path')
+//Añadir el archivo necesario para trabajar con HTML
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    entry: './src/index.js',
+//Creación del modulo que se exportara donde viene cada configuración de lo que va a suceder
+module.exports ={
+    entry: './src/index.js',  //ahi vive el codigo que incial yy de ahi partira
+    //output es  donde se pondra el proyecto ya copilado y estructurado isto para manadarse aproducción
     output: {
-        path: path.resolve(__dirname, dist),
-        filename: 'main.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
     },
     resolve: {
-        extensions : ['.js'],
+        extensions: ['.js'],
     },
     module: {
         rules: [
             {
                 test: /\.js?$/,
-                exclude : /node_modules/,
-                use : {
-                    loader: 'babel-loader'
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
                 }
             }
         ]
     },
-    plugins: [
+    plugins:[
         new HtmlWebpackPlugin([
-            {
-                inject: true,
-                template: './public/index.html',
-                filename: './index.html',
-            }
-        ])
+        {
+            inject: true,
+            template: './public/index.html',
+            flename: './index.html'
+        }
+        ]),
     ]
 }
